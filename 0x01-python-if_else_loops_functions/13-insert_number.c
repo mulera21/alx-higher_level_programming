d78e430478a3a792692632cd61e8aab498af55ba
edmond mulera <edmondmulera55@gmail.com>
2023-06-06 15:08:21 +0300

    updated

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,32 +1,34 @@
-
 #include "lists.h"
 
-listint_t* insert_node(listint_t** head, int number)
+/**
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - a pointer to the new node.
+ */
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t* new_node = (listint_t*)malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-	}
-	new_node->data = number;
-	new_node->next = NULL;
+	new->n = number;
 
-	if (*head == NULL || number < (*head)->data)
+	if (node == NULL || node->n >= number)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = node;
+		*head = new;
+		return (new);
 	}
 
-	listint_t* current = *head;
+	while (node && node->next && node->next->n < number)
+		node = node->next;
 
-	while (current->next != NULL && current->next->data < number)
-	{
-		current = current->next;
-	}
+	new->next = node->next;
+	node->next = new;
 
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	return (new);
 }
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for char in str:
-        if ord(char) => 97 and  ord(char) <= 122:
+        if ord(char) >= 97 and  ord(char) <= 122:
             char = chr(ord(char) -32)
             print("{}".format(char), end=' ')
     print(" ")
