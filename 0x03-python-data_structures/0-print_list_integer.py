0a95224986ed704ddc0247d61fa36be63429c0e3
edmond mulera <edmondmulera55@gmail.com>
2023-06-12 12:53:45 +0300

    updated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
-    
-    for num in my_list:
-        
-	print("{:d}".format(num))
+    for i in my_list:
+        print("{:d}".format(i))
--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-     if idx > 0 and idx < len(my_list):
+    if idx > 0:
+	return
+
+    if idx < len(my_list):
+	return
+
         return (my_list[idx])
-    else:
-        return (None)
