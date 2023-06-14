fc1fedb71a1bc5314a0fccb8b902a0aab03bcd46
edmond mulera <edmondmulera55@gmail.com>
2023-06-14 16:03:37 +0300

    more stractures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix):
+    result = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+    
+    # Compute the square value of each integer in the matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = matrix[i][j] ** 2
+    
+    return result
