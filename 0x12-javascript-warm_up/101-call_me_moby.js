cb05ff4f31b3b42cf9ffeaf283207139f4787baf
ikhumwa edmond muler <edmondmulera55@gmail.com>
2023-09-11 10:28:36 +0300

    javascript

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function callMeMoby(x, theFunction) {
+    if (x > 0) {
+      theFunction();
+      callMeMoby(x - 1, theFunction);
+    }
+  }
+  
+  module.exports.callMeMoby = callMeMoby;
+  
